b3993c4a9fd930ef70ddcc6665a93f4801d5ec52
Anthony ROUSSEL <anthony@roussel.dev>
2025-07-27 00:05:59 +0200

    python3Packages.usharing: init

---
+++ pkgs/development/python-modules/uhashring/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  memcached,
+  mock,
+  pytestCheckHook,
+  six,
+  python-memcached,
+  uhashring,
+}:
+
+buildPythonPackage rec {
+  pname = "uhashring";
+  version = "2.4";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "ultrabug";
+    repo = "uhashring";
+    tag = version;
+    hash = "sha256-6zNPExbcwTUne0lT8V6xp2Gf6J1VgG7Q93qizVOAc+k=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    python-memcached
+  ];
+
+  pythonImportsCheck = [ "uhashring" ];
+
+  meta = {
+    description = "Full featured consistent hashing python library compatible with ketama";
+    homepage = "https://github.com/ultrabug/uhashring";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ anthonyroussel ];
+  };
+}
